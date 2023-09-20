13e70d6e2dcab8e59c3f8b7fbaf9e040f21a6258
yuka00000 <yukayamada0914@gmail.com>
2023-09-20 05:42:13 +0000

    [Add]model:Item,CartItem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_12_084640) do
+ActiveRecord::Schema.define(version: 2023_09_20_053829) do
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.text "explanation"
+    t.boolean "is_active"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
