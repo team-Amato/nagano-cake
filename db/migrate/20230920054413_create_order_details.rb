5500539bd6c950d085c4e3cf3a0d5ddb279edf7c
EC2 Default User <ec2-user@ip-172-31-31-120.ap-northeast-1.compute.internal>
2023-09-20 05:54:14 +0000

    [Add]model:Order,OrderDetail

---
+++ db/migrate/20230920054413_create_order_details.rb
@@ -0,0 +1,11 @@
+class CreateOrderDetails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_details do |t|
+      t.integer :purchase_price
+      t.integer :amount
+      t.integer :making_status
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_12_084640) do
+ActiveRecord::Schema.define(version: 2023_09_20_054413) do
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "purchase_price"
+    t.integer "amount"
+    t.integer "making_status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "post_code"
+    t.string "address"
+    t.string "name"
+    t.integer "payment_method"
+    t.integer "status"
+    t.integer "postage"
+    t.integer "total_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
