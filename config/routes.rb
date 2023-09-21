c274dda68e8ac19ce7f8d1411cf847de0e728956
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-09-21 04:06:50 +0000

    [Add]item,genre,customer controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+  get 'customers/show'
+  get 'customers/edit'
+  get 'genres/index'
+  get 'genres/edit'
+  get 'items/index'
+  get 'items/new'
+  get 'items/show'
+  get 'items/edit'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
