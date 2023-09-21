50f5d1102a02c1f423e15e9b419807e05e7d92a7
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-09-21 04:24:04 +0000

    [Finish]adminのorders,order_details,sessions,homes作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
+  namespace :admin do
+    get 'order_details/update'
+  end
+  namespace :admin do
+    get 'orders/show'
+    get 'orders/update'
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
