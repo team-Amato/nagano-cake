e9a848410aa83b8c1159b2d524cff978dff070b6
EC2 Default User <ec2-user@ip-172-31-31-120.ap-northeast-1.compute.internal>
2023-09-21 03:55:30 +0000

    [Add]sessions,customers,cart_itams,odrders,controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  get 'orders/new'
+  get 'orders/confirm'
+  get 'orders/thanks'
+  get 'orders/create'
+  get 'orders/index'
+  get 'orders/show'
+  get 'cart_items/index'
+  get 'cart_items/update'
+  get 'cart_items/destroy'
+  get 'cart_items/destroy_all'
+  get 'cart_items/create'
+  get 'customers/show'
+  get 'customers/edit'
+  get 'customers/update'
+  get 'customers/check'
+  get 'customers/withdraw'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2023_09_20_074011) do
 
-ActiveRecord::Schema.define(version: 2023_09_20_064314) do
-
-  create_table "cart_items", force: :cascade do |t|
-    t.integer "amount", null: false
+  create_table "addresses", force: :cascade do |t|
+    t.string "address", null: false
+    t.string "post_code", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", null: false
@@ -25,13 +27,11 @@ ActiveRecord::Schema.define(version: 2023_09_20_064314) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "items", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "price", null: false
-    t.text "explanation", null: false
-    t.boolean "is_active", null: false
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "last_name", null: false
@@ -48,4 +48,51 @@ ActiveRecord::Schema.define(version: 2023_09_20_064314) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "explanation", null: false
+    t.boolean "is_active", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "purchase_price", null: false
+    t.integer "amount", null: false
+    t.integer "making_status", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.integer "payment_method", null: false
+    t.integer "status", null: false
+    t.integer "postage", null: false
+    t.integer "total_price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
