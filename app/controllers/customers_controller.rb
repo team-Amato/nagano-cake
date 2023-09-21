61da5608adda9990f1b494d17d5bf6bffc181c5f
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-09-21 04:30:48 +0000

    [Add]item,genre,customer controller追加

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,19 @@
+class CustomersController < ApplicationController
+  def new
+  end
+  
+  def index
+  end
+  
+  def create
+  end  
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+  end  
+end
