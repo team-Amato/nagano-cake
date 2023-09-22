af1db04cc301d1f8e4afff5eae7f02c8eff52a76
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-09-22 07:58:44 +0000

    [Add]管理者ログイン

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -7,4 +7,14 @@ class Admin::SessionsController < ApplicationController
 
   def destroy
   end
+  
+  protected
+  def after_sign_in_path_for(resource)
+    admin_items_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: {
+  sessions: 'admins/sessions'
+}
   devise_for :users
   devise_for :customers
   
