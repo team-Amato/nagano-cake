87d63906b9af7b973d658e0dca11341ce142524e
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-09-23 07:11:05 +0000

    Topページ&Aboutページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :customers
-
+devise_for :customers
   root to: "homes#top"
   get '/about' => "homes#about"
-
-  get '/customers/sign_up' => 'registrations#new'
-  post '/customers' => 'registrations#create'
-
-  get '/customers/sign_in' => 'sessions#new'
-  post '/customers/sign_in' => 'sessions#create'
-  delete '/customers/sign_out' => 'sessions#destroy'
-
   get '/customers/mypage' => 'customers#show'
-  get '/customers/edit' => 'customers#edit'
-  patch '/customers' => 'customers#update'
   get 'customers/check'
   patch 'customers/withdraw'
-
+  get '/genre/search' => 'searches#genre_search'
   resources :cart_items, only: [:index, :update, :destroy, :create]
   delete 'cart_items/destroy_all'
-
   resources :orders, only: [:new, :create, :index, :show]
   post 'orders/confirm'
   get 'orders/thanks'
-
   resources :items, only: [:index, :show]
-
   resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-
   #admin
   namespace :admin do
     get '/admin/sign_in' => 'sessions#new'
