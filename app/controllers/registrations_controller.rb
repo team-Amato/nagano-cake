deac1e2db36b785583adb893ca03df8cbf6815a3
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-09-23 04:44:57 +0000

    [Update]管理者画面

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,17 @@
 class RegistrationsController < ApplicationController
   def new
+    @user = Admin.new
   end
 
   def create
+    @user = Admin.new(email: params[:email], password: params[:password])
+    if @user.save
+      flash[:notice] = "会員登録完了あなたは#{@user.id}人目のサービス利用者です(´・ω・`)"
+      redirect_to("/")
+    else
+      flash[:alert] = "会員登録失敗"
+      render action: :new
+    end
+
   end
 end
