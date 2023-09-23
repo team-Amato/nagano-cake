d12566b1e5f0628a976d062822fe30574e1955fb
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-09-23 09:45:01 +0000

    商品一覧(顧客側)機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,12 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @item_counts = @items.count
   end
 
   def show
+    @item = Item.find(params[:id])
   end
+
+
 end
